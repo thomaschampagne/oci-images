3599f54e2be90b3255ad93b9fc6b2d6cc75ec4c4
Thomas Champagne <th.champagne@gmail.com>
2025-12-28 11:28:31 +0100

    feat: add pihole

---
+++ pihole/exec.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+APP_NAME=pihole
+TAG=develop
+IMAGE=thomaschampagne/oci-$APP_NAME:$TAG
+PORT=80
+
+cd "$(dirname "$0")" || exit
+
+# Init clear
+opt_clear=false
+
+if [[ "$1" = "-h" || "$1" = "--help" ]]; then
+  echo "Usage: "
+  echo "    -c|--clear Drop existing image & volumes"
+  exit 2
+fi
+
+if [[ "$1" = "-c" || "$1" = "--clear" ]]; then
+  opt_clear=true
+fi
+
+# Kill existing container if exists
+docker rm -f $APP_NAME
+
+# Clear if requested
+if [[ "$opt_clear" == "true" ]]; then
+    docker image rm $IMAGE
+    docker volume rm oci-$APP_NAME-data 2> /dev/null
+fi
+
+# Build image
+docker build -t $IMAGE . && docker image prune -f
+
+# Create required volume
+docker volume create oci-$APP_NAME-data 2> /dev/null
+
+# Run
+docker run -dit \
+    --name $APP_NAME \
+    -p $PORT:$PORT \
+    $IMAGE
+
+echo -e "\n[Info] Container running. Access app at: http://localhost:$PORT"
+echo -e "\n[Info] Connect to container with: docker exec -it $APP_NAME sh\n\n"
+docker logs -f $APP_NAME
